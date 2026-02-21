75c0080154faa011f2b7b4f6816e79f9e1e0424a
Your Name <you@example.com>
2026-02-21 14:39:35 +0300

    New functions in bst.h

--- src/bst.h
+++ src/bst.h
@@ -8,3 +8,13 @@ Tree* newTree();
 void bstInsert(Tree*, int);
 bool bstContains(Tree*, int);
 void bstFree(Tree*);
+
+void bstInorder(Tree*);
+void bstPreorder(Tree*);
+void bstPostorder(Tree*);
+
+int bstHeight(Tree*);
+int bstSize(Tree*);
+// bstMin и bstMax возвращают 0 на пустом дереве
+int bstMin(Tree*);
+int bstMax(Tree*);
