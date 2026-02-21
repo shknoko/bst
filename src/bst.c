fbf2fbd14a723a7dedb2b3bb8d5a6f864ac41b14
vladimirmitnovitskiy <harrellprmime@gmail.com>
2026-02-21 15:10:21 +0300

    add bool bstContain(Tree* tree, int value), void  bstInsert(Tree* tree, int value)

--- src/bst.c
+++ src/bst.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct bstNode {
     int data;
@@ -49,4 +50,66 @@ void freeNode(bstNode* node)
     free(node->rightChild);
     free(node);
     return 0;
+}
+
+bool bstContain(Tree* tree, int value)
+{
+    bstNode* current = tree->root;
+
+    while (current != NULL)
+    {
+        if (value == current->data){
+            return true;
+        }
+        else if (value < current->data){
+            current = current->leftChild;
+        }
+        else if (value > current->data){
+            current = current->rightChild;
+        }
+    }
+
+    return false;
+    
+}
+
+void bstInsert(Tree* tree, int value)
+{
+   
+
+    if (tree->root == NULL){
+        tree->root = newNode(value);
+    }
+
+     bstNode* current = tree->root;
+
+    while (true)
+    {
+        if (value == current->data){
+            return;
+        }
+
+        if (value < current->data){
+            if (current->leftChild = NULL){
+                current->leftChild = newNode(value);
+                return;
+            }
+
+            current = current->leftChild;
+        }
+
+        else {
+            if (current->rightChild == NULL){
+                current->rightChild = newNode(value);
+                return;
+            }
+
+            current = current->rightChild;
+        }
+    }
+}
+
+void bstFree(Tree* tree)
+{
+
 }
