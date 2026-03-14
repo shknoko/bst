1bc371540b7081aa4f26e2b61255856ce9539921
Nadya Shaporova <saporovanada@gmail.com>
2026-03-14 21:56:48 +0300

    fixed style

--- src/bstFG.c
+++ src/bstFG.c
@@ -1,22 +1,25 @@
-#include <stdlib.h>
 #include "bst.h"
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdlib.h>
 
-typedef struct StackNode{
-    bstNode *treeNode;
-    struct StackNode *next;
-}StackNode;
+typedef struct StackNode {
+    bstNode* treeNode;
+    struct StackNode* next;
+} StackNode;
 
-void push(StackNode **top, bstNode* node) {
+void push(StackNode** top, bstNode* node)
+{
     StackNode* newNode = (StackNode*)malloc(sizeof(StackNode));
     newNode->treeNode = node;
     newNode->next = *top;
     *top = newNode;
 }
 
-bstNode* pop(StackNode **top) {
-    if (*top == NULL) return NULL;
+bstNode* pop(StackNode** top)
+{
+    if (!*top)
+        return NULL;
     StackNode* temp = *top;
     bstNode* node = temp->treeNode;
     *top = temp->next;
@@ -24,63 +27,61 @@ bstNode* pop(StackNode **top) {
     return node;
 }
 
-bool bstIsValid(Tree *tree) {
-    if(tree == NULL || tree->root == NULL){
+bool bstIsValid(Tree* tree)
+{
+    if (!tree || !tree->root)
         return true;
-    }
 
-    StackNode *stack = NULL;
-    bstNode *curr = tree->root;
-    bstNode *prevNode = NULL;
-     while(curr != NULL || stack != NULL) {
-        while(curr != NULL) {
+    StackNode* stack = NULL;
+    bstNode* curr = tree->root;
+    bstNode* prevNode = NULL;
+
+    while (curr || stack) {
+        while (curr) {
             push(&stack, curr);
             curr = curr->leftChild;
         }
 
         curr = pop(&stack);
 
-        if(prevNode != NULL && curr->data <= prevNode->data) {
-            while(stack != NULL) pop(&stack);
+        if (prevNode && curr->data <= prevNode->data) {
+            while (stack)
+                pop(&stack);
             return false;
         }
 
         prevNode = curr;
-        curr = curr->rightChild; 
+        curr = curr->rightChild;
     }
     return true;
 }
 
-static bstNode *kthMinRecursive(bstNode *root, int *k)
+static bstNode* kthMinRecursive(bstNode* root, int* k)
 {
-    if(root == NULL){
+    if (!root)
         return NULL;
-    }
-    bstNode *left = kthMinRecursive(root->leftChild, k);
-if (left != NULL){
-    return left;
-}  
 
-(*k)--;
-if (*k == 0){
-    return root;
+    bstNode* left = kthMinRecursive(root->leftChild, k);
+    if (left)
+        return left;
+
+    (*k)--;
+    if (*k == 0)
+        return root;
+
+    return kthMinRecursive(root->rightChild, k);
 }
 
-return kthMinRecursive(root->rightChild, k);
-}
-
-int bstKthMin(Tree *tree, int k)
+int bstKthMin(Tree* tree, int k)
 {
-    if(tree == NULL || tree->root == NULL || k <= 0 || k > bstSize(tree)){
+    if (!tree || !tree->root || k <= 0 || k > bstSize(tree))
         return -1;
-    }
 
-    bstNode *result = kthMinRecursive(tree->root, &k);
-    
-    if(result != NULL){
+    bstNode* result = kthMinRecursive(tree->root, &k);
+
+    if (result)
         return result->data;
-    } else {
-        return -1;
-    }
+    return -1;
 }
+
 
